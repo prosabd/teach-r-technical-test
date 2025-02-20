440a01e66ca5ee586e514aca37755d2a3d8733e5
prosabd <102315258+prosabd@users.noreply.github.com>
2025-02-20 14:28:27 +0100

    feat: :sparkles: refactor Docker setup by removing nginx and supervisor, updating php-fpm configuration, and enhancing entrypoint script

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -5,13 +5,17 @@ echo "Installing Composer dependencies..."
 composer install -vvv --prefer-dist --no-scripts --no-autoloader
 
 echo "Running database migrations..."
-php bin/console doctrine:migrations:migrate -vvv --no-interaction
+php bin/console doctrine:database:create --if-not-exists --connection=default
+php bin/console doctrine:migrations:migrate -n
 
 echo "Loading fixtures..."
-php bin/console doctrine:fixtures:load -vvv --no-interaction
+php bin/console doctrine:fixtures:load -n
 
 echo "Generating JWT keys..."
 php bin/console lexik:jwt:generate-keypair --skip-if-exists --no-interaction
+
+php bin/console assets:install
+chown 1000:1000 -R public
 
 echo "Setup completed successfully!"
 exec "$@"
